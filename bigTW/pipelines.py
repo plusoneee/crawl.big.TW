01780e17a23bf4f1b0f60603b306d5f8bc5bde93
plusoneee <y02121148s@gmail.com>
2018-08-22 13:50:33 +0800

    new func to filter old data

--- bigTW/pipelines.py
+++ bigTW/pipelines.py
@@ -12,11 +12,14 @@ class BigtwPipeline(object):
 class MySqlPipeline(object):
     old_data_from_sql = []
     def open_spider(self, spider):
+
+        # Database Settings
         db = spider.settings.get('MYSQL_DB_NAME')
         host = spider.settings.get('MYSQL_DB_HOST')
         port = spider.settings.get('MYSQL_PORY')
         user = spider.settings.get('MYSQL_USER')
         password = spider.settings.get('MYSQL_PASSWORD')
+        # Database Connecting
         self.connection = pymysql.connect(
             host = host,
             user = user,
@@ -24,21 +27,24 @@ class MySqlPipeline(object):
             db = db,
             cursorclass= pymysql.cursors.DictCursor
         )
+        
+    def close_spider(self, spider):
+        self.connection.close()
+    
+    def process_item(self, item, spider):
+        self.filter_repeat_data(item)
+        return item
 
+    def filter_repeat_data(self, item):
+        # Getting Old Data from DB
         with self.connection.cursor() as cursor:
             sql = "SELECT * FROM newsAll"
             cursor.execute(sql)
             for row in cursor:
                 self.old_data_from_sql.append(row['title'])
-
-    def close_spider(self, spider):
-        # self.connection.commit()
-        self.connection.close()
-    
-    def process_item(self, item, spider):
+        
         if item['title'] not in self.old_data_from_sql:
             self.insert_to_mysql(item)
-        return item
 
     def insert_to_mysql(self, item):
         values = (
